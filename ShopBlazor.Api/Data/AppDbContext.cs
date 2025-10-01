4d56bf9fb86ddef1ba33ae68891e78d1032ce94d
Caique Coelho Pinheiro <caiquepinheiro69@gmail.com>
2025-10-01 14:09:31 -0300

    Setando as tabelas no AppDbContext

---
+++ ShopBlazor.Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopBlazor.Api.Entities;
+
+namespace ShopBlazor.Api.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Cart>? Carts { get; set; }
+        public DbSet<ItemCart>? ItemCarts { get; set; }
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<User>? Users { get; set; }
+    }
+}
