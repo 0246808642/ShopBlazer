263fc0dee323be1fc7974a2c2ef6d6d224adc3df
Caique Coelho Pinheiro <caiquepinheiro69@gmail.com>
2025-10-01 14:35:11 -0300

    Conectando com o banco de dados PostgreSQL

--- ShopBlazor.Api/Program.cs
+++ ShopBlazor.Api/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using ShopBlazor.Api.Data;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Conectando com o banco de dados PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(op=> op.UseNpgsql(builder.Configuration.GetConnectionString("Connection")));
 
 var app = builder.Build();
 
