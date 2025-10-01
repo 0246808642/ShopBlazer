7763a6c3e09a2ddb773cfbf12f162f8adaa8b8a3
Caique Coelho Pinheiro <caiquepinheiro69@gmail.com>
2025-10-01 17:56:14 -0300

    Finalizando o DTO com AutoMapper

---
+++ ShopBlazor.Api/Mapping/MappingDtos.cs
@@ -0,0 +1,18 @@
+﻿using ShopBlazor.Api.Entities;
+using ShopBlazor.Models.DTOs;
+
+namespace ShopBlazor.Api.Mapping;
+
+public class MappingDtos : AutoMapper.Profile
+{
+    public MappingDtos()
+    {
+        CreateMap<Category, CategoryDto>();
+
+        CreateMap<Product, ProductDto>();
+
+        CreateMap<(ItemCart itemCart, Product product), ItemCartDto>();
+
+
+    }
+}
